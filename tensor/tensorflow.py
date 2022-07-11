6f20d92d5d5d26ca76f734fc43b8471c2dc12962
kwon-hyemin <kwon9184h@gmail.com>
2022-07-11 20:07:00 +0900

    0711

--- tensor/tensorflow.py
+++ tensor/tensorflow.py
@@ -1,8 +1,17 @@
 import tensorflow as tf
 
+
+class Tensor:
+    def __int__(self):
+        pass
+
+    def ten(self):
+        pass
+
+
 if __name__ == '__main__':
     tf.debugging.set_log_device_placement(True)
-    a = tf.constant([[1.0,2.0,3.0],[4.0,5.0,6.0]])
-    b = tf.constant([[1.0,2.0],[3.0,4.0],[5.0,6.0]])
-    c = tf.matmul(a,b)
+    a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+    b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+    c = tf.matmul(a, b)
     print(c)
